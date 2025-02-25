6ef832434275d3591ea9274be1856ad61bcce54f
Shubham <ushubham81@gmail.com>
2025-02-25 17:29:14 +0530

    changes to get_locators() now it accept key and return value by key

--- config/locators.py
+++ config/locators.py
@@ -1,4 +1,8 @@
 #locators.py
-def get_locators(url):
+def get_locators(url, key):
     if 'https://opensource-demo.orangehrmlive.com' in url:
-       return {'username': '//input[@name="username"]', 'password': '//input[@name="password"]', 'login_btn': '//button[@type="submit"]'}
+        locators = {'username': '//input[@name="username"]',
+                    'password': '//input[@name="password"]',
+                    'login_btn': '//button[@type="submit"]'
+                    }
+        return locators[key]
--- pages/login_page.py
+++ pages/login_page.py
@@ -8,9 +8,9 @@ from utils.browser_actions import BrowserActions
 class LoginPage(BrowserActions):
     def __init__(self, driver, url):
         self.driver = driver.driver
-        self.username_input = (By.XPATH, locators.get_locators(url)['username'])
-        self.password_input =  (By.XPATH, locators.get_locators(url)['password'])
-        self.login_btn = (By.XPATH, locators.get_locators(url)['login_btn'])
+        self.username_input = (By.XPATH, locators.get_locators(url, 'username'))
+        self.password_input =  (By.XPATH, locators.get_locators(url, 'password'))
+        self.login_btn = (By.XPATH, locators.get_locators(url, 'login_btn'))
 
     def login(self, uname, passwd):
         self.driver.find_element(*self.username_input).send_keys(uname)
