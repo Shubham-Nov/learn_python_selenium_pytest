3b1a7799f835d32318e2bc82b0f653bd8cc30c3c
Shubham <ushubham81@gmail.com>
2025-02-25 17:45:22 +0530

    changed dict name locators to element in locators.py

--- config/locators.py
+++ config/locators.py
@@ -1,8 +1,8 @@
 #locators.py
 def get_locators(url, key):
     if 'https://opensource-demo.orangehrmlive.com' in url:
-        locators = {'username': '//input[@name="username"]',
+        element = {'username': '//input[@name="username"]',
                     'password': '//input[@name="password"]',
                     'login_btn': '//button[@type="submit"]'
                     }
-        return locators[key]
+        return element[key]
